7ca36059630a7cd69f31ece8f4b59892d361fb78
veith <veithz@gmail.com>
2020-11-22 12:19:31 +0100

    fix: Registerbus

--- pkg/microbus/subscribe.go
+++ pkg/microbus/subscribe.go
@@ -16,10 +16,31 @@ func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
 }
 
 func logEvent(data DataEvent) {
-	if viper.GetBool("messaging.log"){
+	if viper.GetBool("messaging.log") {
 		fmt.Printf("Topic: %s;\n", data.Topic)
 	}
 }
+
+func (bus *EventBus)RegisterSubscriptionsOnBus(subscriptionList map[string]func(event DataEvent)) {
+	for eventName, subscriberFunc := range subscriptionList {
+		ch1 := make(chan DataEvent)
+		bus.Subscribe(eventName, ch1)
+		subscriberFunc := subscriberFunc
+		go func() {
+			for {
+				select {
+				case e := <-ch1:
+					logEvent(e)
+					go func() {
+						subscriberFunc(e)
+
+					}()
+				}
+}
+		}()
+	}
+}
+
 
 func RegisterSubscriptionsOnBus(subscriptionList map[string]func(event DataEvent), bus *EventBus) {
 	for eventName, subscriberFunc := range subscriptionList {
